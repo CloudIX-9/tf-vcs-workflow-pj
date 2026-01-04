3ab2014301098f211c09287bd3bb059a13e94df2
CloudIX-9 <nyne.cloud99@gmail.com>
2026-01-04 16:59:01 +0000

    Deploy Back

--- variable.tf
+++ variable.tf
@@ -1,8 +1,8 @@
-# variable "prefix" {
-#   type        = string
-#   description = "Prefix added to names of all resources"
-#   default     = "quickstart"
-# }
+variable "prefix" {
+  type        = string
+  description = "Prefix added to names of all resources"
+  default     = "quickstart"
+}
 
 variable "aws_region" {
   type        = string
@@ -10,8 +10,8 @@ variable "aws_region" {
   default     = "eu-west-2"
 }
 
-# variable "aws_zone" {
-#   type        = string
-#   description = "AWS zone used for all resources"
-#   default     = "eu-west-2a"
-# }
+variable "aws_zone" {
+  type        = string
+  description = "AWS zone used for all resources"
+  default     = "eu-west-2a"
+}
